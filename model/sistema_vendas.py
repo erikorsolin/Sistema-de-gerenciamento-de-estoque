29f6b5f142495afa20933ace808ede3a53fd6024
Erik Orsolin <depaulaerik670@gmail.com>
2023-06-02 14:11:04 -0300

    mudança na classe SistemaVendas para armazenar somente os produtos

--- model/sistema_vendas.py
+++ model/sistema_vendas.py
@@ -1,18 +1,9 @@
 class SistemaVendas:
     def __init__(self):
         self.produtos = []
-        self.clientes = []
-        self.vendas = {}
 
     def get_produtos(self):
         return self.produtos
-    
-    def get_clientes(self):
-        return self.clientes
-    
+        
     def set_produtos(self, produtos):
         self.produtos = produtos
-
-    def set_clientes(self, clientes):
-        self.clientes = clientes
-    
